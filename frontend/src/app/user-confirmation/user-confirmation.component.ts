220f00bfe8de20aca7d419c751dee1e38191fd77
e0406370 <e0406370@u.nus.edu>
2024-04-05 22:07:27 +0800

    Added user confirmation form

--- frontend/src/app/user-confirmation/user-confirmation.component.ts
+++ frontend/src/app/user-confirmation/user-confirmation.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { UserConfirmationService } from './user-confirmation.service';
 
 @Component({
   selector: 'app-user-confirmation',
@@ -6,6 +9,30 @@ import { Component } from '@angular/core';
   styleUrl: './user-confirmation.component.css'
 })
   
-export class UserConfirmationComponent {
+export class UserConfirmationComponent implements OnInit {
+
+  private fb = inject(FormBuilder);
+  private router = inject(Router);
+  private userConfirmationSvc = inject(UserConfirmationService);
+
+  userConfirmationForm!: FormGroup;
+  userID!: string;
+
+  ngOnInit(): void {
+
+    this.userConfirmationForm = this.createConfirmationForm();
+    this.userID = this.userConfirmationSvc.userID;
+  }
+
+  createConfirmationForm(): FormGroup {
+
+    return this.fb.group({
+
+      confirmationCode: this.fb.control<string>('', [
+        Validators.required,
+        Validators.maxLength(6),
+      ])
+    })
+  }
 
 }
