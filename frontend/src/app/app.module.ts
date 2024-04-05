0515587420dbb41d3f6e4a4b9900825ab32225a7
e0406370 <e0406370@u.nus.edu>
2024-04-05 14:56:22 +0800

    Added initial files for user confirmation purposes

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,36 +1,46 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { MaterialModule } from './material/material.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { UserRegistrationComponent } from './user-registration/user-registration.component';
-import { UserRegistrationService } from './user-registration/user-registration.service';
-
-import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { UserConfirmationComponent } from './user-confirmation/user-confirmation.component';
 import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegistrationComponent } from './user-registration/user-registration.component';
+
+import { UserConfirmationService } from './user-confirmation/user-confirmation.service';
+import { UserLoginService } from './user-login/user-login.service';
+import { UserRegistrationService } from './user-registration/user-registration.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    UserConfirmationComponent,
+    UserLoginComponent,
     UserRegistrationComponent,
-    UserLoginComponent
   ],
   imports: [
     BrowserModule,
-    MaterialModule,
-    AppRoutingModule,
+    HttpClientModule,
     ReactiveFormsModule,
-    HttpClientModule
+    AppRoutingModule,
+    MaterialModule,
   ],
-  providers: [UserRegistrationService, provideAnimationsAsync()],
+  providers: [
+    UserConfirmationService,
+    UserLoginService,
+    UserRegistrationService,
+    provideAnimationsAsync(),
+  ],
   bootstrap: [AppComponent],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
   
-export class AppModule { }
+export class AppModule {}
---
+++ frontend/src/app/user-confirmation/user-confirmation.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-confirmation',
+  templateUrl: './user-confirmation.component.html',
+  styleUrl: './user-confirmation.component.css'
+})
+export class UserConfirmationComponent {
+
+}
