f20f33fac40b784481bc18b374239425292adfd1
e0406370 <e0406370@u.nus.edu>
2024-04-16 15:10:19 +0800

    Handle must login functionality in ngOnInit() itself, check for size of bookmarkedEvents !== 0; if yes, display events, if no, present error message

--- frontend/src/app/event-bookmarks/event-bookmarks.component.ts
+++ frontend/src/app/event-bookmarks/event-bookmarks.component.ts
@@ -4,6 +4,8 @@ import { EventCard } from '../models/event';
 import { SessionStore } from '../utility/session.store';
 import { UtilityService } from '../utility/utility.service';
 import { EventBookmarksService } from './event-bookmarks.service';
+import { ERROR_NOT_LOGGED_IN_MESSAGE } from '../utility/constants';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-event-bookmarks',
@@ -13,6 +15,7 @@ import { EventBookmarksService } from './event-bookmarks.service';
   
 export class EventBookmarksComponent implements OnInit {
 
+  private router = inject(Router);
   private sessionStore = inject(SessionStore);
   private utilitySvc = inject(UtilityService);
   private eventBookmarksSvc = inject(EventBookmarksService);
@@ -20,6 +23,11 @@ export class EventBookmarksComponent implements OnInit {
   bookmarkedEvents$!: Observable<EventCard[]>;
 
   ngOnInit(): void {
+
+    if (!this.sessionStore.isLoggedIn()) {
+      this.utilitySvc.generateErrorMessage(ERROR_NOT_LOGGED_IN_MESSAGE);
+      this.router.navigate(['/login']);
+    }
 
     const userID = this.sessionStore.getLoggedID();
     this.bookmarkedEvents$ = this.eventBookmarksSvc.retrieveEventBookmarks(userID);
